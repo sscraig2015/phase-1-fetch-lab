4fe0a92320d294b1c1d343a75063839962fd693c
Sean <craig.sean92@gmail.com>
2021-11-09 11:57:12 -0700

    Completed Assignment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+function fetchBooks() { 
+return fetch("https://anapioficeandfire.com/api/books")  
+.then((resp) => resp.json())
+.then((resp) => renderBooks(resp))
 }
+
+
+
+
+
 
 function renderBooks(books) {
   const main = document.querySelector('main');
